972efd52aec00f9afbca83bbf0f48bedc1f2823b
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-01 14:56:56 +0100

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 for (var i = 1; i <= 100; i++){
     document.getElementById("campo").innerHTML += "<div class=\"quadrato\">"+i+"</div>";
 }
+var quadrato = document.getElementsByClassName("quadrato");
 document.getElementById("campo").addEventListener("click",
 function(event){
+    event.target.classList.toggle("red");
     alert(event.target.innerHTML);
 })
