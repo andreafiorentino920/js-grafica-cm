650d33a970910561dec91b542f1c0f99dacf5df3
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-01 15:21:33 +0100

    Esercizio con numero di caselle prese in input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-for (var i = 1; i <= 100; i++){
+// Esercizio con dato caselle preso in input 
+var c= parseInt(prompt("Da quante caselle vuoi comporre il campo???"));
+for (var i = 1; i <= c; i++){
     document.getElementById("campo").innerHTML += "<div class=\"quadrato\">"+i+"</div>";
 }
-var quadrato = document.getElementsByClassName("quadrato");
 document.getElementById("campo").addEventListener("click",
 function(event){
     event.target.classList.toggle("red");
     alert(event.target.innerHTML);
 })
+
+// Esercizio con 100 caselle
+// for (var i = 1; i <= 100; i++){
+//     document.getElementById("campo").innerHTML += "<div class=\"quadrato\">"+i+"</div>";
+// }
+// document.getElementById("campo").addEventListener("click",
+// function(event){
+//     event.target.classList.toggle("red");
+//     alert(event.target.innerHTML);
+// })
